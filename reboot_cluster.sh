f41a8ee2bcde1eebf71fa3bd3fea8fd49092c44c
Zhidong Brian Zhang <wonderfulzzd@gmail.com>
2023-05-10 06:38:32 +0800

    Add files via upload

---
+++ reboot_cluster.sh
@@ -0,0 +1,5 @@
+ssh ubuntu@rpi1 sudo shutdown -rf now
+ssh ubuntu@rpi2 sudo shutdown -rf now
+ssh ubuntu@rpi3 sudo shutdown -rf now
+ssh ubuntu@rpi4 sudo shutdown -rf now
+sudo shutdown -rf now
---
+++ shutdown_cluster.sh
@@ -0,0 +1,5 @@
+ssh ubuntu@rpi1 sudo shutdown now
+ssh ubuntu@rpi2 sudo shutdown now
+ssh ubuntu@rpi3 sudo shutdown now
+ssh ubuntu@rpi4 sudo shutdown now
+sudo shutdown now
